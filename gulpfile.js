566ffb74bf8b876f737a51ae2f238ae9ccdce49a
erichua <erichua@webank.com>
2017-08-28 11:43:38 +0800

    test version number

--- gulpfile.js
+++ gulpfile.js
@@ -39,22 +39,22 @@ gulp.task('setup', ['check:git', 'check:node'], () => {
 gulp.task('publish:major', shell.task([
   'npm version major',
   'npm publish --access=public',
-  'git push',
-  'git push --tag',
+  'git add package.json',
+  'git push --follow-tags'
 ], {cwd: './lib'}))
 
 gulp.task('publish:minor', shell.task([
   'npm version minor',
   'npm publish --access=public',
-  'git push',
-  'git push --tag',
+  'git add package.json',
+  'git push --follow-tags'
 ], {cwd: './lib'}))
 
 gulp.task('publish:patch', shell.task([
   'npm version patch',
   'npm publish --access=public',
-  'git push',
-  'git push --tag',
+  'git add package.json',
+  'git push --follow-tags'
 ], {cwd: './lib'}))
 
 gulp.task('help', () => {
